247e481d446a7229ef19186aa4a0804bf1b84313
yohrannes.santos <yohrannes.santos@dcide.com.br>
2026-04-17 14:22:49 -0300

    update env vars name

--- create-dns-record.sh
+++ create-dns-record.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-ZONE_ID=$(./list-dns-zones.sh | jq -r --arg name "$DESIRED_DOMAIN_NAME" '.results[] | select(.name==$name) | .id')
+ZONE_ID=$(./list-dns-zones.sh | jq -r --arg name "$DOMAIN" '.results[] | select(.name==$name) | .id')
 
 curl --request POST \
   --url "https://api.azion.com/v4/workspace/dns/zones/${ZONE_ID}/records?page_size=100" \
   --header 'Accept: application/json' \
-  --header "Authorization: Token $AZION_API_TOKEN" \
+  --header "Authorization: Token $PERSONAL_TOKEN" \
   --header 'Content-Type: application/json' \
   --data "{
     \"description\": \"\",
@@ -13,7 +13,7 @@ curl --request POST \
     \"ttl\": 300,
     \"type\": \"CNAME\",
     \"rdata\": [
-      \"${DESIRED_DOMAIN}\"
+      \"${TARGET_DOMAIN}\"
     ],
     \"policy\": \"simple\",
     \"weight\": 255
